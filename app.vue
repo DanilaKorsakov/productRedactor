f80259d5a27b4122382a981a612b378487fa7ce6
DannilaKorsakov <kvashnindan@gmail.com>
2022-07-28 23:52:50 +0300

    Fonts, normalize.css

--- app.vue
+++ app.vue
@@ -3,3 +3,11 @@
 
   </div>
 </template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap');
+  body{
+    font-family: 'Source Sans Pro', sans-serif;
+    color: #3F3F3F;
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 
+    css:[
+        '@/node_modules/normalize.css/normalize.css'
+    ]
 })
